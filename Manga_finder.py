86cf7434eda911469be695d9c9b78ac5aaba4bb9
Brady <100500972+NoWhere-2-Go@users.noreply.github.com>
2022-07-23 19:50:38 -0500

    Create Manga_finder.py

---
+++ Manga_finder.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+
+def get_lang():
+    valid = False
+    languages = ["english", "spanish", "japanese", "german", "french", "italian", "russian", "chinese", "korean"]
+
+    # check if language user typed is valid
+    while not valid:
+        lang = input("What language are you looking for? ")
+        for langs in languages:
+            if langs == lang.lower():
+                return lang
+        print("Enter a valid language")
+
+
+if __name__ == "__main__":
+    manga = input("What manga do you want to search for? ")
+    language_preference = get_lang()
+
+    ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png=1"
+    page = requests.get(ebay_url).text
+    soup = BeautifulSoup(page, "html.parser")
+
+    # make sure to scrape every page that has manga listings
+    x = 1
+    manga_listings = []
+
+    while x <= 3:
+        x = x + 1
+        ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png={x}"
+        title_tags = soup.find_all('h3', class_='s-item__title')
+        price_tags = soup.find_all('span', class_='s-item__price')
+
+        # lets store all manga listings inside a list
+        for i in range(1, len(title_tags)):
+            manga_listings.append({'Title': title_tags[i].text, 'Price': float(price_tags[i].text[1:])})
+
+    # write the list into a pandas dataframe
+    sorted_manga_listings = sorted(manga_listings, key=lambda d: d['Price'])
+    print(*sorted_manga_listings, sep='\n')
+    df = pd.DataFrame.from_dict(manga_listings)
+    df.sort_values(by="Price")
+    #df.to_csv("ebay_manga_postings.csv")
