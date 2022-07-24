863a74c3172d97d28d877a4314583b1d417b3479
Brady <100500972+NoWhere-2-Go@users.noreply.github.com>
2022-07-23 19:59:40 -0500

    Update Manga_finder.py

--- Manga_finder.py
+++ Manga_finder.py
@@ -14,13 +14,31 @@ def get_lang():
             if langs == lang.lower():
                 return lang
         print("Enter a valid language")
+        
+def get_price_min():
+    price = int(input("What is your minimum price? "))
+    while price < 0:
+        print("Enter a valid price")
+        price = int(input("What is your minimum price? "))
+    return price
+
+
+def get_price_max(minimum):
+    price = int(input("What is your maximum price? "))
+    while price <= minimum:
+        print("Enter a valid price")
+        price = int(input("What is your maximum price? "))
+    return price
 
 
 if __name__ == "__main__":
     manga = input("What manga do you want to search for? ")
     language_preference = get_lang()
+    min_price = get_price_min()
+    max_price = get_price_max(min_price)
 
-    ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png=1"
+
+    ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png=1&_udlo={min_price}&_udhi={max_price}"
     page = requests.get(ebay_url).text
     soup = BeautifulSoup(page, "html.parser")
 
@@ -30,7 +48,7 @@ if __name__ == "__main__":
 
     while x <= 3:
         x = x + 1
-        ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png={x}"
+        ebay_url = f"https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={manga}+manga+{language_preference}&_png={x}&_udlo={min_price}&_udhi={max_price}"
         title_tags = soup.find_all('h3', class_='s-item__title')
         price_tags = soup.find_all('span', class_='s-item__price')
 
